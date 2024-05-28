17d53b5aad25f5b2381bc514150739ae10a7432b
Sten Schmidt <sten.schmidt@uscca.com>
2024-05-28 17:20:02 -0500

    first one

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?", (req, res) => {
+  if (req) {
+    res.json({unix: Number(req)});
+  };
+});
+
 
 
 // Listen on port set in environment variable or default to 3000
